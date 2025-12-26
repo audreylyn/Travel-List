fe512afcde0a879ad98b49478671806f8ee55a0b
audreylyn <audreylynvillanueva1504@gmail.com>
2025-12-26 23:58:02 +0800

    section 7: Clearing the list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Socks", quantity: 12, packed: true },
-];
-
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Socks", quantity: 12, packed: true },
+// ];
 
 export default function App() {
     const [items, setItems] = useState([]);
+
 
     function handleAddItems(item) {
         setItems((items) => [...items, item]);
     }
 
+    function handleDeleteItem(id) {
+        setItems((items) => items.filter((item) => item.id !== id));
+    }
+
+    function handleToggleItem(id) {
+        setItems((items) => items.map((item) => item.id === id ? {...item, packed: !item.packed } : item ));
+    }
+
+    function handleClearLists() {
+        const confirmed = window.confirm(
+            "Are you sure you want to delete all items?"
+        )
+        if(confirmed) setItems([]);
+    }
+
     return <div className="app">
         <Logo/>
         <Form onAddItems={handleAddItems} />
-        <PackingList items={items} />
-        <Stats/>
+        <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearLists={handleClearLists}/>
+        <Stats items={items}/>
     </div>
 }
 
@@ -67,30 +82,63 @@ function Form({onAddItems}) {
     );
 }
 
-function PackingList({items}) {
+function PackingList({items, onDeleteItem, onToggleItem, onClearLists}) {
+
+    const [sortBy, setSortBy] = useState("description");
+
+    let sortedItems;
+
+    if(sortBy === "input") sortedItems = items;
+    if(sortBy === "description") sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+    if (sortBy == "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+
+
     return <div className="list">
         <ul>
-            {items.map((item) =>  (
-                <Item item={item} key={item.id}/>
+            {sortedItems.map((item) =>  (
+                <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>
             ))}
         </ul>
+
+        <div className="actions">
+            <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                <option value="input">Sort by input order</option>
+                <option value="description">Sort by description</option>
+                <option value="packed">Sort by packed status</option>
+            </select>
+
+            <button onClick={onClearLists}>Clear List</button>
+        </div>
     </div>;
 }
 
-function Item({item}) {
+function Item({item, onDeleteItem, onToggleItem}) {
     return (
         <li>
+            <input type="checkbox" checked={item.packed} onChange={() => onToggleItem(item.id)}/>
             <span style={item.packed ? {textDecoration: "line-through"} : {}}>
                 {item.quantity} {item.description}
             </span>
-            <button>❌</button>
+            <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
     )
 }
 
-function Stats() {
+function Stats({items}) {
+
+    if(!items.length) 
+    return (
+            <footer className="stats">
+                <em>Start adding some items to your packing list</em>
+            </footer>
+    );
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
     return (
     <footer className="stats">
-        <em>💼 You have X items on your list, and you already packed X (X%)</em>
+        <em>{percentage === 100 ? "You got everything, Ready to go!" : 
+        `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+            </em>
     </footer>);
 }
